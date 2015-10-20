5d0cff261eed85a49cb5608012760cdcd98e61bc
Ivan Gontarenko <g.ivan@dizboard.com>
2015-10-20 19:39:13 +0300

    in progress

--- src/Cli/IO/Input.php
+++ src/Cli/IO/Input.php
@@ -8,6 +8,89 @@
      */
     abstract class Input {
 
+        protected $runningFile  = '';
 
+        protected $tokens       = [];
+
+        protected $arguments    = [];
+
+        protected $options      = [];
+
+        public function __construct() {
+            $arguments  = $_SERVER['argv'];
+            $this->setRunningFile( array_shift( $arguments ) );
+            $this->setTokens( $arguments );
+            $this->parse();
+        }
+
+        /**
+         * @return string
+         */
+        public function getRunningFile()
+        {
+            return $this->runningFile;
+        }
+
+        /**
+         * @param string $runningFile
+         * @return static
+         */
+        public function setRunningFile( $runningFile ) {
+            $this->runningFile = $runningFile;
+            return $this;
+        }
+
+        /**
+         * @return array
+         */
+        public function getTokens() {
+            return $this->tokens;
+        }
+
+        /**
+         * @param array $tokens
+         * @return static
+         */
+        public function setTokens( $tokens ) {
+            $this->tokens = $tokens;
+            return $this;
+        }
+
+        /**
+         * @return array
+         */
+        public function getArguments()
+        {
+            return $this->arguments;
+        }
+
+        /**
+         * @param $name
+         * @param $argument
+         * @return static
+         */
+        public function setArgument( $name, $argument ) {
+            $this->arguments[$name] = $argument;
+            return $this;
+        }
+
+        /**
+         * @return array
+         */
+        public function getOptions() {
+            return $this->options;
+        }
+
+        /**
+         * @param $name
+         * @param $value
+         * @return static
+         */
+        public function setOption( $name, $value ) {
+            $this->options[$name] = ! $value ? true : $value;
+            return $this;
+        }
+
+        abstract protected function parse();
 
     }
--- src/Cli/IO/InputArgv.php
+++ src/Cli/IO/InputArgv.php
@@ -4,6 +4,47 @@
 
     class InputArgv extends Input {
 
+        protected function parse() {
 
+            $tokens  = $this->getTokens();
+
+            if( count( $tokens ) > 0 ) {
+
+                $index  = 0;
+
+                for( $index = 0, $length = count( $tokens ); $index < $length; $index++ ) {
+
+                    $token  = $tokens[ $index ];
+
+                    if( strpos( $token, '--' ) === 0 && $token !== '--' ) {
+
+                        list( $name, $value )   = explode( '=', $token );
+                        $this->setOption( substr( $name, 2 ), $value );
+
+                    } else if ( strpos( $token, '-' ) === 0 && $token !== '-' ) {
+
+                        $name       = substr( $token, 1 );
+                        $nextValue  = $tokens[ $index + 1 ];
+
+                        if( $nextValue !== null && strpos( $nextValue, '-' ) !== 0 ) {
+                            $index++;
+                            $this->setOption( $name[0], $nextValue );
+                        } else {
+                            $this->setOption( $name[0], substr( $name, 1 ) );
+                        }
+
+                    } else {
+                        $this->setArgument( $index, $token );
+                    }
+
+                }
+
+                unset( $tokens );
+
+            }
+
+            return $this;
+
+        }
 
     }
